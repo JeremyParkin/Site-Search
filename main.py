71b72ea5157a3b3473f8470ace44a39b2d7dae51
Jeremy Parkin <djplana@gmail.com>
2022-04-25 09:27:53 -0400

    Updated site list

--- main.py
+++ main.py
@@ -3,17 +3,39 @@ import webbrowser
 from datetime import date
 from datetime import timedelta
 
+st. set_page_config(layout="wide")
+
 today = date.today()
 yesterday = today - timedelta(days = 1)
 
-websites = ['onemileatatime.com',
-            'creditcardgenius.ca',
-            # 'www.greedyrates.ca',
-            # 'princeoftravel.com',
-            # 'www.pointsnerd.ca',
-            # 'travelupdate.com',
-            # 'www.flyertalk.com',
-            'www.executivetraveller.com']
+
+websites = ['flytrippers.com',
+'winstonsih.com/category/travel',
+'milesopedia.com/en/category/news',
+'www.moneywehave.com',
+'travelupdate.com/blogger/pointsmilesandbling',
+'www.pointsnerd.ca',
+'www.pointsnerd.ca',
+'princeoftravel.com/blog',
+'onemileatatime.com',
+'www.rewardscanada.ca',
+'www.ratehub.ca/blog',
+'www.nerdwallet.com',
+'www.greedyrates.ca',
+'creditcardgenius.ca/blog',
+'www.godsavethepoints.com',
+'pizzainmotion.boardingarea.com',
+'thepointsguy.co.uk/news',
+'thepointsguy.com/all',
+'www.travelcodex.com',
+'viewfromthewing.com',
+'blog.wandr.me',
+'canadapointsguy.com',
+'awardwallet.com',
+'thriftytraveler.com',
+'www.cnn.com/cnn-underscored',
+'www.cnbc.com/select',
+'www.google.ca']
 
 st.header("Aeroplan: Key Site Search")
 
@@ -37,9 +59,14 @@ if submitted:
     start_date = date_range[0].strftime("%m/%d/%Y")
     end_date = date_range[1].strftime("%m/%d/%Y")
 
+    all_links = ""
     for website in websites:
-        target_url = f'https://www.google.com/search?as_sitesearch={website}&q={keywords}&tbs=cdr:1,cd_min:{start_date},cd_max:{end_date}'
-        # st.write(target_url)
+        target_url = f'www.google.com/search?as_sitesearch={website}&q={keywords}&tbs=cdr:1,cd_min:{start_date},cd_max:{end_date}'
+        # all_links += target_url + '\n'
+        st.write(target_url)
         webbrowser.open_new_tab(target_url)
+
+    # st.write(all_links)
+
 
 
